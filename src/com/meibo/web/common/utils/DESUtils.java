77041ab9c9e394f0f2cdea5ce52a57c379ff2899
ziqing.chen <chenziqing@juxinli.com>
2017-11-21 21:05:38 +0800

    test

--- src/com/meibo/web/common/utils/DESUtils.java
+++ src/com/meibo/web/common/utils/DESUtils.java
@@ -1,56 +1,49 @@
 package com.meibo.web.common.utils;
 
-import java.lang.reflect.Method;
-import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.spec.AlgorithmParameterSpec;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
-import sun.misc.BASE64Encoder;
-
 public class DESUtils {
 
-	public static String SHA1(String decript) {
+	public static String SHA1( String decript ) {
 		try {
 			MessageDigest digest = java.security.MessageDigest
-					.getInstance("SHA-1");
-			digest.update(decript.getBytes());
+					.getInstance( "SHA-1" );
+			digest.update( decript.getBytes() );
 			byte messageDigest[] = digest.digest();
 			// Create Hex String
 			StringBuffer hexString = new StringBuffer();
 			// 字节数组转换为 十六进制 数
-			for (int i = 0; i < messageDigest.length; i++) {
-				String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
-				if (shaHex.length() < 2) {
-					hexString.append(0);
+			for ( int i = 0; i < messageDigest.length; i++ ) {
+				String shaHex = Integer.toHexString( messageDigest[i] & 0xFF );
+				if ( shaHex.length() < 2 ) {
+					hexString.append( 0 );
 				}
-				hexString.append(shaHex);
+				hexString.append( shaHex );
 			}
 			return hexString.toString();
 
-		} catch (NoSuchAlgorithmException e) {
+		} catch ( NoSuchAlgorithmException e ) {
 			e.printStackTrace();
 		}
 		return "";
 	}
 
-	public static void main(String[] args) {
+	public static void main( String[] args ) {
 
-		String sha1Str = SHA1("SH2543");
-		sha1Str = sha1Str.toUpperCase().substring(0, 8);
-		System.out.println(sha1Str); // B7506071
+		String sha1Str = SHA1( "SH2543" );
+		sha1Str = sha1Str.toUpperCase().substring( 0, 8 );
+		System.out.println( sha1Str ); // B7506071
 
-		String s = encrypt("userPass=SH2543".getBytes(), "B7506071", "B7506071");
-		System.out.println(s);
+		String s = encrypt( "userPass=SH2543".getBytes(), "B7506071",
+				"B7506071" );
+		System.out.println( s );
 		// String params = "userPass=SH2543";
 		// String postUrl =
 		// "http://4g.1069106.com:9898/FlowAPI/GetProductsInfo.ashx";
@@ -72,40 +65,40 @@ public class DESUtils {
 	 *            String
 	 * @return byte[]
 	 */
-	public static String encrypt(byte[] datasource, String password, String iv) {
+	public static String encrypt( byte[] datasource, String password, String iv ) {
 		try {
 			SecureRandom random = new SecureRandom();
-			DESKeySpec desKey = new DESKeySpec(password.getBytes());
-			IvParameterSpec ivSpec = new IvParameterSpec(iv.getBytes());
+			DESKeySpec desKey = new DESKeySpec( password.getBytes() );
+			IvParameterSpec ivSpec = new IvParameterSpec( iv.getBytes() );
 			// 创建一个密匙工厂，然后用它把DESKeySpec转换成
-			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-			SecretKey securekey = keyFactory.generateSecret(desKey);
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance( "DES" );
+			SecretKey securekey = keyFactory.generateSecret( desKey );
 			// Cipher对象实际完成加密操作
-			Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+			Cipher cipher = Cipher.getInstance( "DES/CBC/PKCS5Padding" );
 			// 用密匙初始化Cipher对象
-			cipher.init(Cipher.ENCRYPT_MODE, securekey, ivSpec);
+			cipher.init( Cipher.ENCRYPT_MODE, securekey, ivSpec );
 			// 现在，获取数据并加密
 			// 正式执行加密操作
-			byte[] ret = cipher.doFinal(datasource);
+			byte[] ret = cipher.doFinal( datasource );
 			return bytesToHexString( ret );
-		} catch (Throwable e) {
+		} catch ( Throwable e ) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	public static String bytesToHexString(byte[] src) {
-		StringBuilder stringBuilder = new StringBuilder("");
-		if (src == null || src.length <= 0) {
+
+	public static String bytesToHexString( byte[] src ) {
+		StringBuilder stringBuilder = new StringBuilder( "" );
+		if ( src == null || src.length <= 0 ) {
 			return null;
 		}
-		for (int i = 0; i < src.length; i++) {
+		for ( int i = 0; i < src.length; i++ ) {
 			int v = src[i] & 0xFF;
-			String hv = Integer.toHexString(v);
-			if (hv.length() < 2) {
-				stringBuilder.append(0);
+			String hv = Integer.toHexString( v );
+			if ( hv.length() < 2 ) {
+				stringBuilder.append( 0 );
 			}
-			stringBuilder.append(hv);
+			stringBuilder.append( hv );
 		}
 		return stringBuilder.toString();
 	}
@@ -117,17 +110,17 @@ public class DESUtils {
 	 *            the hex string
 	 * @return byte[]
 	 */
-	public static byte[] hexStringToBytes(String hexString) {
-		if (hexString == null || hexString.equals("")) {
+	public static byte[] hexStringToBytes( String hexString ) {
+		if ( hexString == null || hexString.equals( "" ) ) {
 			return null;
 		}
 		hexString = hexString.toUpperCase();
 		int length = hexString.length() / 2;
 		char[] hexChars = hexString.toCharArray();
 		byte[] d = new byte[length];
-		for (int i = 0; i < length; i++) {
+		for ( int i = 0; i < length; i++ ) {
 			int pos = i * 2;
-			d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+			d[i] = ( byte ) ( charToByte( hexChars[pos] ) << 4 | charToByte( hexChars[pos + 1] ) );
 		}
 		return d;
 	}
@@ -139,8 +132,8 @@ public class DESUtils {
 	 *            char
 	 * @return byte
 	 */
-	private static byte charToByte(char c) {
-		return (byte) "0123456789ABCDEF".indexOf(c);
+	private static byte charToByte( char c ) {
+		return ( byte ) "0123456789ABCDEF".indexOf( c );
 	}
 
 }
